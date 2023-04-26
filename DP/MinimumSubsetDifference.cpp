6de277e5a74eb147f2d3c8d0234d79ba6fb5d186
BhomaRamprajapat <bhoma07062001@gmail.com>
2023-04-26 19:57:46 +0530

    commit

---
+++ DP/MinimumSubsetDifference.cpp
@@ -0,0 +1,18 @@
+Promila walks 10 km towards North. She turns right and walks another 10 km. She turns right and walks another 10 km. In which direction is she from the starting point?
+
+
+
+a.
+North
+
+
+b.
+East
+
+
+c.
+South
+
+
+d.
+West
